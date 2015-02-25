e852f2fda150a37c90814c2e63055dff56076862
Courtney Nguyen <courtney@localhost.localdomain>
2015-02-25 01:44:48 -0700

    circular logic tests

--- js/college-courses.js
+++ js/college-courses.js
@@ -1,33 +1,83 @@
-/* Test if classes have spaces or have ""
-
-
-*/
-var _ = require('../node_modules/underscore/underscore.js');
 module.exports = (function(){
 	var courseCatalog = [];
+	var _ = require('../node_modules/underscore/underscore.js');
 
-	// $('#registration').submit(function(e){
-	// 	var courseList = $('#courses').val();
-	// 	parseClasses(courseList);
-	// 	e.preventDefault();
-	// 	return false;	
-	// });
 
 	function parseClasses(unparsedString){
 		var courses = [];
 
 		_.each(unparsedString, function(course){
-		var colonlessString = course.split(':');
-		courses.push({"class": colonlessString[0].trim(), "prerequisite": colonlessString[1].trim()});
+			var colonlessString = course.split(':');
+			courses.push({"class": colonlessString[0].trim(), "prerequisite": colonlessString[1].trim()});
 		});
 
 		return courses;
+	}
+
+//I could also assign indexes to classes, but I would need to keep track of which ones already have been entered.
+// As long as classes do not have more than 1 prerequisite and are not repeated.
+function recursiveCourses(courses){
+
+	var recursiveCourses = [];
+
+	for(var x = 0; x < courses.length; x++){
+		var isCircular = false;
+		console.log(courses[x]);
+		if(hasPrerequisite(courses[x])){
+			var hasMorePrerequisites = true;
+			var findPrerequisite = courses[x].prerequisite;
+				var listOfPrerequisites = []; // I store the class names and if any prerequisite matches up, it is circular logic, b/c there is only 1. 
+				while(hasMorePrerequisites && !isCircular){
+					var course = _.findWhere(courses, {"class": findPrerequisite});
+					if(!hasPrerequisite(course))hasMorePrerequisites = false;
+					else{
+						findPrerequisite = course.prerequisite;
+						if(listOfPrerequisites.indexOf(findPrerequisite) < 0){
+							listOfPrerequisites.push(findPrerequisite);
+						}
+						else{
+							for(var y = 0; y < listOfPrerequisites.length; y++){
+								if(listOfPrerequisites[y] === findPrerequisite){
+									isCircular = true;
+									recursiveCourses.push(listOfPrerequisites[y]);
+									break;
+								}
+							}
+						}
+
+					}
+				}
+			}
+		}
+		return recursiveCourses;
+	}
+
+	function containsCircularLogic(courses){
+		var test = recursiveCourses(courses).length > 0 ? true : false;
+		return test;
+	}
+
+	function hasPrerequisite(course){
+		// console.log('================== hasPrerequisite() ==================');
+		if(course === undefined || course.prerequisite === undefined) return false;
+		
+		// console.log(course["class"] + " ++++ " + course.prerequisite);
+		return !(course.prerequisite.trim() === "");
 	}
 
 	return {
 		"courseCatalog": this.courseCatalog,
 		"parseClasses": function(classes){
 			return parseClasses(classes);
+		},
+		"hasPrerequisite": function(test){
+			return hasPrerequisite(test);
+		},
+		"containsCircularLogic": function(test){
+			return containsCircularLogic(test);
+		},
+		"recursiveCourses": function(courses){
+			return recursiveCourses(courses);
 		}
 	};
 
--- test/college-courses_test.js
+++ test/college-courses_test.js
@@ -1,6 +1,3 @@
-'use strict';
-
-
 var jquery = require('../bower_components/jquery/dist/jquery.min.js');
 var collegeCourses = require('../js/college-courses.js');
 var data = require('../data.json');
@@ -11,13 +8,34 @@ describe('collegeCourses', function () {
 
   it('should parse string', function () {
   	var courses = collegeCourses.parseClasses(data.unparsedData);
-  	console.log(courses);
-  	// console.log(data);
-    // collegeCourses.parseClasses(data.unparsedData);
+
     expect(courses[0]).to.have.property('class');
     expect(courses[0]["class"]).to.equal(data.parsedData[0]["class"]);
     expect(courses[0]["prerequisite"]).to.equal(data.parsedData[0]["prerequisite"]);
     expect(courses[3]["prerequisite"]).to.equal(data.parsedData[3]["prerequisite"]);
   });
+
+  it('should test for prerequisites', function(){
+  	var hasPrerequisite = collegeCourses.hasPrerequisite(data.parsedData[6]);
+  	expect(hasPrerequisite).to.equal(false);
+  	hasPrerequisite = collegeCourses.hasPrerequisite(data.parsedData[0]);
+  	expect(hasPrerequisite).to.equal(true);
+  });
+
+  it('should have no circular logic', function(){
+  	var noCircularLogic = collegeCourses.containsCircularLogic(data.parsedData);
+  	expect(noCircularLogic).to.equal(false);
+  });
+
+  it('should have a list of circular logic courses', function(){
+  	var count = collegeCourses.recursiveCourses(data.circularData);
+  	console.log(count.length);
+  	expect(count.length).to.be.above(0);
+  });
+
+  // it('should have circular logic', function(){
+  // 	var yesCircularLogic = collegeCourses.containsCircularLogic(data.circularData);
+  // 	expect(yesCircularLogic).to.equal(true);
+  // });
 
 });
